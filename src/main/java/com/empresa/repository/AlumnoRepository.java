a57b96468c54e879c660e76e9e4b9acf55b97c1b
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-08-31 00:44:40 -0500

    ajustes

--- src/main/java/com/empresa/repository/AlumnoRepository.java
+++ src/main/java/com/empresa/repository/AlumnoRepository.java
@@ -1,8 +1,8 @@
-package com.cibertec.repository;
+package com.empresa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cibertec.entity.Alumno;
+import com.empresa.entity.Alumno;
 
 public interface AlumnoRepository extends JpaRepository<Alumno, Integer>{
 	
